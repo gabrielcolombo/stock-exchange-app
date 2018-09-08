419ffcb35bae0ddb26bbc4be9c0196d975a3dab8
Gabriel Colombo <gabriel.colombo@framesoft.com.br>
2018-09-08 17:39:52 -0300

    15th Chapter - Creating a Connection Factory

---
+++ client/app/util/ConnectionFactory.js
@@ -0,0 +1,58 @@
+const ConnectionFactory = (() => {
+  const stores = ['negotiations'];
+  let connection = null;
+  let close = null;
+
+  return class ConnectionFactory {
+    constructor() {
+      throw new Error('This class should not be instantiated.');
+    }
+
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        if(connection) {
+          return resolve(connection);
+        }
+        
+        const openRequest = indexedDB.open('jscangaceiro', 2);
+
+        openRequest.onupgradeneeded = event => {
+          ConnectionFactory._createStores(event.target.result);
+        }
+    
+        openRequest.onsuccess = event => {
+          connection = event.target.result;
+          close = connection.close.bind(connection);
+
+          connection.close = () => {
+            throw new Error('A connection should not be closed directly. Use ConnectionFactory.closeConnection() instead.');
+          }
+
+          resolve(event.target.result);
+        }
+    
+        openRequest.onerror = event => {
+          reject(event.target.error.name);
+        }
+      });
+    }
+
+    static _createStores(connection) {
+      stores.forEach((store) => {
+        if(connection.objectStoreNames.contains(store)) {
+          connection.deleteObjectStore(store);
+        }
+
+        connection.createObjectStore(store, {
+          autoIncrement: true,
+        });
+      });
+    }
+
+    closeConnection() {
+      if(connection) {
+        close();
+      }
+    }
+  }
+})();
